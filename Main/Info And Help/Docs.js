15ae613b93642b6e77e261dd2291aafed7d5dc0f
Mr.Respected <77716214+Kudos-Official@users.noreply.github.com>
2021-10-08 15:16:14 +0530

    Delete Main directory

--- Main/Info And Help/Docs.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({
-name:"docs",
-aliases:"documentation",
-code:`$apimessage[$channelid;Click the button for the link! Thanks for using me <3;;{actionRow:Documentation,2,5,$replacetext[https://app.gitbook.com/@mrrespected/s/advanced-suggestions-docs/;:;#COLON#]};$messageID:true;no]`})
--- Main/Info And Help/Mention Bot(Mobile).js
+++
@@ -1,4 +0,0 @@
-module.exports = ({
-name:"<@876074903873388564>",
-code:`Hi **$usertag**, my prefix in **$servername** is \`$getservervar[prefix]\`, and \`$getservervar[prefix]help\` is a great way to get started!`,
-nonPrefixed:true})
