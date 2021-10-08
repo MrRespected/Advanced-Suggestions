e2b4135367286db50555b4aac13b49bdebc1755e
Mr.Respected <77716214+Kudos-Official@users.noreply.github.com>
2021-10-08 15:18:58 +0530

    Create Mention bot(mobile).js

---
+++ Commands/Main/Mention bot(mobile).js
@@ -0,0 +1,5 @@
+module.exports = ({
+name:"<@!876074903873388564>",
+code:`Hi **$usertag**, my prefix in **$servername** is \`$getservervar[prefix]\`, and \`$getservervar[prefix]help\` is a great way to get started!`,
+nonPrefixed:true
+})
